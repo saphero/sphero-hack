3c0e603709ecd4d97eadb62978e78d03201ad2a3
Logan Tegman <ltegman@gmail.com>
2016-01-31 10:50:10 -0800

    Add basic socket backend

--- temp_server.js
+++ temp_server.js
@@ -1,7 +1,26 @@
+'use strict';
+
 const express = require('express');
-const app = express();
+const app = express()();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('connect-sphero', () => {
+    // call to connect to sphero
+  });
+  socket.on('connect-bb8', () => {
+    // call to connect to bb8
+  });
+  socket.on('robot-action', () => {
+    // Send generic command to sphero
+    // orb[opts.type].apply(orb, [...opts.args,
+    //   () => event.sender.send('performed action: ' + opts.type)]);
+  });
+});
 
 app.set('views', __dirname + '/views');
 app.get('/', (req, res) => {
   res.render('index.jade');
-}).listen(3000, () => console.log('Server running on port 3000'));
+});
+server.listen(3000, () => console.log('Server running on port 3000'));
