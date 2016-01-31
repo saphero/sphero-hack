c596a60d218cf20a46c9948af7f34075800c2557
Jose Tello <josectello@gmail.com>
2016-01-31 12:04:27 -0800

    setup static routing and a test landing page...

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,21 @@
 const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 const mocha = require('gulp-mocha');
+const sass = require('gulp-sass');
 
 var files = ['index.js', 'gulpfile.js', './lib/*.js', './test/*.spec.js',
- '!node_modules/**', '!*.json'];
+ '!node_modules/**', '!*.json', './views/scss/**/*.scss'];
 
 gulp.task('lint', () => {
   return gulp.src(files)
     .pipe(eslint())
     .pipe(eslint.format());
+});
+
+gulp.task('sass', () => {
+  return gulp.src('views/scss/style.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('public/css'));
 });
 
 gulp.task('mocha', () => {
@@ -19,7 +26,7 @@ gulp.task('mocha', () => {
 });
 
 gulp.task('watch', () => {
-  return gulp.watch(files, ['lint', 'mocha']);
+  gulp.watch(files, ['sass', 'lint', 'mocha']);
 });
 
 gulp.task('default', ['watch', 'lint', 'mocha']);
--- temp_server.js
+++ temp_server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 
 app.set('views', __dirname + '/views');
+
+app.use('/static', express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
   res.render('index.jade');
 }).listen(3000, () => console.log('Server running on port 3000'));
