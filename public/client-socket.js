9afca4c8e418e67251e31c5211575d6157f5703f
Logan Tegman <ltegman@gmail.com>
2016-02-01 10:46:15 -0800

    Add some commands and lint/refactor

--- public/client-socket.js
+++ public/client-socket.js
@@ -1,9 +1,16 @@
-var socket = io.connect('http://localhost:3000');
+'use strict';
 
-document.getElementById('spheroSetup').onclick = () => socket.emit('setup-sphero');
-document.getElementById('spheroConnect').onclick = () => socket.emit('connect-sphero');
+/* eslint-disable no-undef */
+var socket = io.connect('http://localhost:3000');
+/* eslint-enable no-undef */
+
+document.getElementById('spheroSetup').onclick =
+  () => socket.emit('setup-sphero');
+document.getElementById('spheroConnect').onclick =
+  () => socket.emit('connect-sphero');
 document.getElementById('bb8Setup').onclick = () => socket.emit('setup-bb8');
-document.getElementById('bb8Connect').onclick = () => socket.emit('connect-bb8');
+document.getElementById('bb8Connect').onclick =
+  () => socket.emit('connect-bb8');
 document.getElementById('lights').onclick = () => socket.emit('flashingLights');
 
 function sendAction(socket, actions) {
