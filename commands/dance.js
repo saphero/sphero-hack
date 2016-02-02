d6b2e7dd95c2270e169bf312b3fa85e7fac2c6f5
jessethach <jessethach@gmail.com>
2016-02-01 18:18:19 -0800

    initiating test folder

---
+++ commands/dance.js
@@ -0,0 +1,18 @@
+// "use strict";
+//
+// var sphero = require("../");
+// var orb = sphero(process.env.PORT);
+//
+// orb.connect(function() {
+//   setInterval(function() {
+//   var direction = Math.floor(Math.random() * 360);
+//   bb8.roll(150, direction);
+// }, 1000);
+//
+// };
+//
+// var danceRandomHex = function() {
+//   var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
+// };
+
+//randomColor function as an export
