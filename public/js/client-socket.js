28b61f0cd31177d8dc0feb9c19109f927c956148
Jose Tello <josectello@gmail.com>
2016-02-02 13:05:12 -0800

    fix merge conflicts...

---
+++ public/js/client-socket.js
@@ -0,0 +1,50 @@
+'use strict';
+
+/* eslint-disable no-undef */
+var socket = io.connect('http://localhost:3000');
+/* eslint-enable no-undef */
+
+document.getElementById('spheroConnect')
+  .onclick = () => socket.emit('connect-sphero');
+document.getElementById('disconnect')
+  .onclick = () => socket.emit('disconnect');
+document.getElementById('bb8Setup').onclick = () => socket.emit('setup-bb8');
+document.getElementById('bb8Connect')
+  .onclick = () => socket.emit('connect-bb8');
+document.getElementById('lights')
+  .onclick = () => socket.emit('flashing-lights');
+document.getElementById('keyboardControls').onclick = toggleKeyControls();
+
+function toggleKeyControls() {
+  if (!window.onkeydown) {
+    window.onkeydown = e => {
+      switch (e.keyCode) {
+        case 32:
+          socket.emit('roll', 'stop');
+          break;
+        case 37:
+          socket.emit('roll', 'left');
+          break;
+        case 38:
+          socket.emit('roll', 'up');
+          break;
+        case 39:
+          socket.emit('roll', 'right');
+          break;
+        case 40:
+          socket.emit('roll', 'down');
+          break;
+        case 79:
+          socket.emit('speed', 'down');
+          break;
+        case 80:
+          socket.emit('speed', 'up');
+          break;
+        default:
+          // do nothing
+      }
+    };
+  } else {
+    window.onkeydown = null;
+  }
+}
