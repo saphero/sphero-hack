33ec57a8ffeb0c3c8cf6d1e65e0aa6d28bdfc62f
Jose Tello <josectello@gmail.com>
2016-02-01 19:24:50 -0800

    add testing for static routes, close server after tests are done

---
+++ test/router.spec.js
@@ -0,0 +1,47 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+const expect = chai.expect;
+const serverInst = require(__dirname + '/../server');
+
+describe('UNIT: test the router endpoints', () => {
+  it('should make a GET request at / (landing page)', (done) => {
+    chai.request('localhost:3000')
+      .get('/')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('should make a GET request at /about', (done) => {
+    chai.request('localhost:3000')
+      .get('/about')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('should make a GET request at /color', (done) => {
+    chai.request('localhost:3000')
+      .get('/color')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  it('should make a GET request at /move', (done) => {
+    chai.request('localhost:3000')
+      .get('/move')
+      .end((err, res) => {
+        expect(err).to.eql(null);
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+  after(done => {
+    serverInst.close(done);
+  });
+});
