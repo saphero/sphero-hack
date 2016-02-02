7d179cb698e96267f829c69bf09234e9419b253f
jessethach <jessethach@gmail.com>
2016-02-02 05:38:04 -0800

    fixed syntax issues and removed superagent

--- commands/dance.js
+++ commands/dance.js
@@ -5,11 +5,10 @@
 //
 // orb.connect(function() {
 //   setInterval(function() {
-//   var direction = Math.floor(Math.random() * 360);
-//   bb8.roll(150, direction);
-// }, 1000);
-//
-// };
+//     var direction = Math.floor(Math.random() * 360);
+//     orb.roll(150, direction);
+//   }, 1000);
+// });
 //
 // var danceRandomHex = function() {
 //   var randomColor = "#000000".replace(/0/g,function(){return (~~(Math.random()*16)).toString(16);});
