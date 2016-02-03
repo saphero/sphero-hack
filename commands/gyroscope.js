b0fd7dcedfb1b946274de9bbf66704dd11741b49
jessethach <jessethach@gmail.com>
2016-02-02 16:02:59 -0800

    added gyroscope

---
+++ commands/gyroscope.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = exports = (orb) => {
+  orb.streamGyroscope();
+
+  orb.on("gyroscope", function(data) {
+    console.log("gyroscope:");
+    console.log("    value:", data.xGyro.value[0]);
+    console.log("    value:", data.yGyro.value[0]);
+    console.log("    value:", data.zGyro.value[0]);
+  });
+};
