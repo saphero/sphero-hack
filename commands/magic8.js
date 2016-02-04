869697fee97c39a169d28cc02aedb26bd1aab019
Logan Tegman <ltegman@gmail.com>
2016-02-04 14:14:23 -0800

    Add tests

--- commands/magic8.js
+++ commands/magic8.js
@@ -1,36 +1,32 @@
 'use strict';
 var start = false;
 module.exports = exports = (orb) => {
-  orb.connect(() => {
-    orb.color('purple');
+  orb.color('purple');
 
-    orb.streamVelocity();
+  orb.streamVelocity();
 
-    orb.on('velocity', (data) => {
-      console.log(data.xVelocity.value[0], data.yVelocity.value[0]);
-      if (data.xVelocity.value[0] > 100 || data.yVelocity.value[0] > 100) {
-        console.log('shaking');
-        start = true;
-      } else {
-        if (start) {
-          predict();
-          start = false;
-        }
-      }
-    });
-
-    function predict() {
-      var number = Math.random() * 3;
-      if (number < 1) {
-        console.log('YES');
-        return orb.color('green');
-      }
-      if (number < 2) {
-        console.log('MAYBE');
-        return orb.color('yellow');
-      }
-      console.log('NO');
-      return orb.color('red');
+  orb.on('velocity', (data) => {
+    console.log(data.xVelocity.value[0], data.yVelocity.value[0]);
+    if (data.xVelocity.value[0] > 100 || data.yVelocity.value[0] > 100) {
+      console.log('shaking');
+      start = true;
+    } else if (start) {
+      predict();
+      start = false;
     }
   });
+
+  function predict() {
+    var number = Math.random() * 3;
+    if (number < 1) {
+      console.log('YES');
+      return orb.color('green');
+    }
+    if (number < 2) {
+      console.log('MAYBE');
+      return orb.color('yellow');
+    }
+    console.log('NO');
+    return orb.color('red');
+  }
 };
