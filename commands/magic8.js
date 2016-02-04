e4beddce54571c6fa0316385305d3eaab4192f16
SABRINA TEE <sabrinatee@me.com>
2016-02-03 23:49:59 -0800

    Fixed logic operators

--- commands/magic8.js
+++ commands/magic8.js
@@ -20,15 +20,15 @@ module.exports = exports = (orb) => {
 
     function predict() {
       var number = Math.random() * 3;
-      if (0 <= number && number < 1) {
+      if (number < 1) {
         console.log('YES');
         return orb.color('green');
       }
-      if (1 <= number && number < 2) {
+      if (number < 2) {
         console.log('MAYBE');
         return orb.color('yellow');
       }
-      if (2 <= number && number < 3) {
+      if (number <= 3) {
         console.log('NO');
         return orb.color('red');
       }
