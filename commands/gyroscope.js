fb54e4e2a7a716359dd1879c3ece662e8dce5d48
Jose Tello <josectello@gmail.com>
2016-02-03 21:35:35 -0800

    create blank gyroscope graph, need sphero to properly test

--- commands/gyroscope.js
+++ commands/gyroscope.js
@@ -1,6 +1,6 @@
 'use strict';
 
-module.exports = exports = (orb) => {
+module.exports = exports = (orb, socket) => {
   orb.streamGyroscope();
 
   orb.on('gyroscope', (data) => {
@@ -8,5 +8,15 @@ module.exports = exports = (orb) => {
     console.log('    value:', data.xGyro.value[0]);
     console.log('    value:', data.yGyro.value[0]);
     console.log('    value:', data.zGyro.value[0]);
+    var dataArr = [[]];
+    if (!data.xGyro.value[0] || !data.yGyro.value[0] || !data.zGyro.value[0]) {
+      dataArr[0].push([0, 0]);
+    } else {
+      dataArr.push([0, data.xGyro.value[0]]);
+      dataArr.push([1, data.yGyro.value[0]]);
+      dataArr.push([2, data.zGyro.value[0]]);
+    }
+    socket.emit('gyroscope', dataArr);
+    console.log(dataArr);
   });
 };
